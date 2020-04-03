bcdc2ae4c66a3abf37255ae098d9050fd0d0db6f
Jinyoung Jeong <jinyoung.jeong20@gmail.com>
2020-04-03 15:45:08 +0100

    temporary update for mobile version

--- app/src/scripts/app.js
+++ app/src/scripts/app.js
@@ -4,63 +4,55 @@ let canvas = document.querySelector("canvas")
 let canvasContext2d = canvas.getContext("2d") 
 let canvasWidth = canvas.width = window.innerWidth 
 let canvasHeight = canvas.height = window.innerHeight
- 
+
 let form = document.querySelector('form')
 let text = form.querySelector(".text")
 let textMessage = text.value 
-    
 let mouse = {x: undefined, y: undefined}
 
-function Particle(x, y, r, accX, accY){
-  this.x = randomIntFromRange(r, canvasWidth-r)
-  this.y = randomIntFromRange(r, canvasHeight-r)
-  this.r = r
+canvasContext2d.fillStyle = "black"
+
+
+
+function Particle(x, y, accX, accY){
+  this.x = x
+  this.y = y
   this.color = "black"
   this.velocity = {x: randomIntFromRange(-10, 10), y: randomIntFromRange(-10, 10)}
   this.dest = {x : x, y : y}
-  this.accX = 5;
-  this.accY = 5;
-  this.accX = accX;
-  this.accY = accY;
+  this.accX = 5
+  this.accY = 5
+  this.accX = accX
+  this.accY = accY
   this.friction = randomNumDecimal(0.94, 0.98)
   
-
+ 
   this.draw = function(){    
     canvasContext2d.beginPath()
-    canvasContext2d.fillRect(this.x, this.y, 2, 2);
-    canvasContext2d.fillStyle = "black"
+    canvasContext2d.fillRect(this.x, this.y, 2, 2)
     canvasContext2d.fill()
-    canvasContext2d.closePath() 
-  }
+  } 
 
   this.update = function(){
     this.draw()
-
-    if(this.x + this.r > canvasWidth || this.x - this.r < 0){
-      this.velocity.x = -this.velocity.x
-    }
-
-    if(this.y + this.r > canvasHeight || this.y - this.r < 0){
-      this.velocity.y = -this.velocity.y
-    }
  
     this.accX = (this.dest.x - this.x) / 300;
     this.accY = (this.dest.y - this.y) / 300;
 
-    this.velocity.x += this.accX;
-    this.velocity.y += this.accY;
+    this.velocity.x += this.accX
+    this.velocity.y += this.accY
 
-    this.velocity.x *= this.friction;
-    this.velocity.y *= this.friction;
+    this.velocity.x *= this.friction
+    this.velocity.y *= this.friction
 
-    this.x += this.velocity.x;
-    this.y += this.velocity.y; 
+    this.x += this.velocity.x
+    this.y += this.velocity.y 
 
     if(dist(this.x, this.y, mouse.x, mouse.y) < 70){
-      this.accX = (this.x - mouse.x) / 30;
-      this.accY = (this.y - mouse.y) / 30;
-      this.velocity.x += this.accX;
-      this.velocity.y += this.accY;
+      this.accX = (this.x - mouse.x) / 30
+      this.accY = (this.y - mouse.y) / 30
+      this.velocity.x += this.accX
+      this.velocity.y += this.accY
     }
   }
 }
@@ -71,7 +63,7 @@ let numParticles
 function init(){
   particles = []
 
-  canvasContext2d.font = `bold ${canvasWidth/10}px sans-serif`;
+  canvasContext2d.font = `bold ${canvasWidth/10}px sans-serif`
   canvasContext2d.textAlign = "center"
   canvasContext2d.fillText(textMessage, canvasWidth/2, canvasHeight/2)
 
@@ -83,11 +75,10 @@ function init(){
       if(data[((canvasWidth * j + i) * 4) + 3]){
         let x = i + randomNumDecimal(0, 3)
         let y = j + randomNumDecimal(0, 3) 
-        let r = randomNumDecimal(1, 1.5)
         let accX = randomNumDecimal(-3, 0.2)
         let accY = randomNumDecimal(-3, 0.2)
 
-        particles.push(new Particle(x, y, r, accX, accY))
+        particles.push(new Particle(x, y, accX, accY))
       } 
     }
   }
@@ -102,6 +93,7 @@ function animate(){
   for(let i = 0; i < numParticles; i++){
     particles[i].update()
   } 
+   
   requestAnimationFrame(animate)
 }
 
@@ -121,41 +113,30 @@ form.addEventListener('submit', function(e){
   animate()
 })
 
-
-function resizeToMatchDisplaySize(canvas) {
-  canvasWidth  = canvas.clientWidth  * window.devicePixelRatio;
-  canvasHeight = canvas.clientHeight * window.devicePixelRatio;
- if (canvas.width !== canvasWidth || canvas.height !== canvasHeight) {
-   canvas.width  = canvasWidth;
-   canvas.height = canvasHeight;
-   return true;
- }
- return false;
-}
-
-function randomIntFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-} 
-
-function randomNumDecimal(min, max) {
-  return Math.random() * (max - min) + min
-}
-
-function dist(x1, y1, x2, y2){ 
-  let xDist = x1 - x2
-  let yDist = y1 - y2
-
-  return Math.sqrt( (xDist * xDist) + (yDist * yDist))
-} 
-
 window.addEventListener('resize', function() {
+    console.log("resizing..")
     canvasWidth = canvas.width = window.innerWidth
     canvasHeight = canvas.height = window.innerHeight  
          
-     init() 
+    init() 
  }) 
 
 window.addEventListener('mousemove', function(e){
   mouse.x = e.clientX
   mouse.y = e.clientY
 })
+
+function randomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  } 
+  
+function randomNumDecimal(min, max) {
+return Math.random() * (max - min) + min
+}
+
+function dist(x1, y1, x2, y2){ 
+let xDist = x1 - x2
+let yDist = y1 - y2
+
+return Math.sqrt( (xDist * xDist) + (yDist * yDist))
+} 
